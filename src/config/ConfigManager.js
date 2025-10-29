e590a7970fb777e373ec5566a5a7c1e1423bc8aa
chris <ge47jaz@mytum.de>
2025-10-29 21:38:52 +0100

    simplified LogService logic

--- src/config/ConfigManager.js
+++ src/config/ConfigManager.js
@@ -242,7 +242,7 @@ export class ConfigManager {
     loadCPEEConfig() {
         return {
             wfadaptor: {
-                themePath: 'src/libs/cpee-layout/themes/presetid/theme.js', // replaced: 'src/libs/cpee-layout/themes/preset/theme.js'
+                themePath: 'src/libs/cpee-layout/themes/presetid/theme.js', // KEPT LOCAL FOR NOW!
                 cssPath: 'https://cpee.org/flow/css/wfadaptor.css', // replaced: 'src/libs/cpee-layout/wfadaptor.css'
                 baseThemePath: 'https://cpee.org/flow/themes/base.js', // replaced: 'src/libs/cpee-layout/themes/base.js'
                 wfadaptorPath: 'https://cpee.org/flow/js/wfadaptor.js' // replaced: 'src/libs/cpee-layout/wfadaptor.js'
