b0979c082222444692ef1871ec8d6303442fddc7
chris <ge47jaz@mytum.de>
2026-01-04 01:41:27 +0100

    added instances to 101461

--- scripts/fetch-uuids.ps1
+++ scripts/fetch-uuids.ps1
@@ -23,7 +23,8 @@ public class TrustAllCertsPolicy : ICertificatePolicy {
 # Process numbers to fetch UUIDs for (from ConfigManager.js)
 # previous: 8618..99344
 # previous: 73190..99428
-$processNumbers = @(99428..101454) # | Where-Object { $_ -ne 98647 }
+# previous: 99328..101454
+$processNumbers = @(101454..101464) # | Where-Object { $_ -ne 98647 }
 
 # Output file
 $outputFile = "scripts\temp\uuids\uuid-mapping.json"
